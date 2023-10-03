830d8edb30099481146e7e247155f905a98ee804
Liaksej <tarusov.liaksej@gmail.com>
2023-10-03 23:01:22 +0200

    Enhance home page display with CardTemplates
    Updated home page layout by utilizing a new CardTemplate component which allows for consistent content formatting. The CardTemplate is equipped with CardMedia and CardContent from the @mui/material library, some headers, bodies and images are used as examples. This provides a more visually appealing way to present information to the users. Also created a new CardTemplate component file which defines the children prop, including button action. This modular approach makes future updates and modifications more manageable.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,39 @@
-import Image from "next/image";
+import { CardTemplate } from "@/components/CardTemplate";
+import { CardContent, CardMedia, Typography } from "@mui/material";
+
+const headers = ["Card title", "Special title treatment"];
+
+const bodies = [
+  "Some quick example text to build on the card title and make up the bulk of the card's content.",
+  "With supporting text below as a natural lead-in to additional content.",
+];
+
+const images = ["https://via.placeholder.com/150"];
 
 export default function Home() {
-  return null;
+  return (
+    <div>
+      <CardTemplate>
+        <CardMedia
+          sx={{ height: 140 }}
+          image={images[0]}
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography variant="h6" component="h2">
+            {headers[0]}
+          </Typography>
+          <Typography variant="body2">{bodies[0]}</Typography>
+        </CardContent>
+      </CardTemplate>
+      <CardTemplate>
+        <CardContent>
+          <Typography variant="h6" component="h2">
+            {headers[1]}
+          </Typography>
+          <Typography variant="body2">{bodies[1]}</Typography>
+        </CardContent>
+      </CardTemplate>
+    </div>
+  );
 }
---
+++ src/components/CardTemplate.tsx
@@ -0,0 +1,27 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { ReactNode } from "react";
+
+interface CardTemplateProps {
+  children: ReactNode;
+}
+
+export const CardTemplate = ({ children }: CardTemplateProps) => {
+  return (
+    <Box className="w-20 mx-auto p-4" sx={{ minWidth: 275 }}>
+      <Card variant="outlined">
+        {children}
+        <CardActions>
+          <Button size="small">Go somewhere</Button>
+        </CardActions>
+      </Card>
+    </Box>
+  );
+};
